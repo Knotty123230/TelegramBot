53fddffe5c6f6843562c639467da6261a6285084
Knotty123230 <122396662+Knotty123230@users.noreply.github.com>
2023-05-25 17:06:03 +0300

    interface

--- src/main/java/button/service/SaveButton.java
+++ src/main/java/button/service/SaveButton.java
@@ -3,19 +3,22 @@ package button.service;
 import User.User;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import java.util.List;
+import User.UserInfo;
 
 
 public class SaveButton {
-    public static String getSave(Update update){
+    public static String getSave(Update update) {
         String callbackData = "";
         List<User> info = UserInfo.getInfo(update.getCallbackQuery().getMessage().getChatId(), update.getCallbackQuery().getData());
         for (User user : info) {
             long userChatid = user.getChatid();
             String callbackDataSince = user.getCallbackDataSince();
-            if (update.getCallbackQuery().getMessage().getChatId().equals(userChatid)){
-                if (update.getCallbackQuery().getData().equals(callbackDataSince));
-                callbackData = callbackDataSince;
+            if (update.getCallbackQuery().getMessage().getChatId().equals(userChatid)) {
+                if (update.getCallbackQuery().getData().equals(callbackDataSince)) {
+                    callbackData = callbackDataSince;
+                }
             }
+
         }
         return callbackData;
     }
