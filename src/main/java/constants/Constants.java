7e66411da27bea4ffb8cca9c62b3304cd3eb00ce
Knotty123230 <122396662+Knotty123230@users.noreply.github.com>
2023-05-28 18:35:07 +0300

    interface

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -4,7 +4,7 @@ package constants;
 
 public class Constants {
 
-    public static final String TOKEN = "6286472591:AAHzm1f-Jhzb8pZLsKi9QLHJF3JGTJ4y5Sw\n";
+    public static final String TOKEN = "6286472591:AAHzm1f-Jhzb8pZLsKi9QLHJF3JGTJ4y5Sw";
     public static final String USERNAME = "projectvaluesbot";
 
 
