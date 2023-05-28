64f54d660d7c73b38e3d01ae2c3254b6897b10f7
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-05-28 18:33:19 +0300

    final clava

--- src/main/java/bot/KeyboardRow.java
+++ src/main/java/bot/KeyboardRow.java
@@ -17,20 +17,47 @@ public class KeyboardRow {
 
     public static SendMessage getUpdate(org.telegram.telegrambots.meta.api.objects.Update update) {
 
+
+        List<String> nameOfButtons = List.of(
+                "9",
+                "10",
+                "11",
+                "12",
+                "13",
+                "14",
+                "15",
+                "16",
+                "17",
+                "18",
+                "Вимкнути повідомлення");
+        List<String> callback = List.of(
+                PageLabels.notificationTime9,
+                PageLabels.notificationTime10,
+                PageLabels.notificationTime11,
+                PageLabels.notificationTime12,
+                PageLabels.notificationTime13,
+                PageLabels.notificationTime14,
+                PageLabels.notificationTime15,
+                PageLabels.notificationTime16,
+                PageLabels.notificationTime17,
+                PageLabels.notificationTime18,
+                PageLabels.notificationTimeOff);
+
+
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
         org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow keyboardRow =
                 new org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow();
-        keyboardRow.addAll(List.of("9","10","11"));
+        keyboardRow.addAll(List.of(nameOfButtons.get(0),nameOfButtons.get(1),nameOfButtons.get(2)));
         org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow keyboardRow1 =
                 new org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow();
-        keyboardRow1.addAll(List.of("12","13","14"));
+        keyboardRow1.addAll(List.of(nameOfButtons.get(3),nameOfButtons.get(4),nameOfButtons.get(5)));
         org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow keyboardRow2 =
                 new org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow();
-        keyboardRow1.addAll(List.of("15","16","17"));
+        keyboardRow2.addAll(List.of(nameOfButtons.get(6),nameOfButtons.get(7),nameOfButtons.get(8)));
         org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow keyboardRow3 =
                 new org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow();
-        keyboardRow3.addAll(List.of("18","Вимкнути повідомлення"));
+        keyboardRow3.addAll(List.of(nameOfButtons.get(9),nameOfButtons.get(10)));
         List<org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow> row = new ArrayList<>();
         row.add(keyboardRow);
         row.add(keyboardRow1);
--- src/main/java/bot/NotificationTimePage.java
+++ src/main/java/bot/NotificationTimePage.java
@@ -3,94 +3,44 @@ package bot;
 import constants.PageLabels;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import service.BotService;
 import service.Update;
 import uIElements.buttons.ButtonService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class NotificationTimePage implements Update {
     @Override
     public SendMessage getUpdate(org.telegram.telegrambots.meta.api.objects.Update update) {
+        List<String> nameOfButtons = List.of(
+                "9",
+                "10",
+                "11",
+                "12",
+                "13",
+                "14",
+                "15",
+                "16",
+                "17",
+                "18",
+                "Вимкнути повідомлення");
+        List<String> callback = List.of(
+                PageLabels.notificationTime9,
+                PageLabels.notificationTime10,
+                PageLabels.notificationTime11,
+                PageLabels.notificationTime12,
+                PageLabels.notificationTime13,
+                PageLabels.notificationTime14,
+                PageLabels.notificationTime15,
+                PageLabels.notificationTime16,
+                PageLabels.notificationTime17,
+                PageLabels.notificationTime18,
+                PageLabels.notificationTimeOff);
 
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
 
-        List<KeyboardRow> nameOfButtons = List.of();
-
-        KeyboardRow row = new KeyboardRow();
-
-        row.add("9");
-        row.add("10");
-        row.add("11");
-
-        nameOfButtons.add(row);
-
-        row = new KeyboardRow();
-
-        row.add("12");
-        row.add("13");
-        row.add("14");
-
-        nameOfButtons.add(row);
-
-        row = new KeyboardRow();
-
-        row.add("15");
-        row.add("16");
-        row.add("17");
-
-        nameOfButtons.add(row);
-
-        row = new KeyboardRow();
-
-        row.add("18");
-        row.add("Вимкнути повідомлення");
-
-        nameOfButtons.add(row);
-
-        keyboardMarkup.setKeyboard(nameOfButtons);
-
-        List<KeyboardRow> callback = List.of();
-
-        row.add(PageLabels.notificationTime9);
-        row.add(PageLabels.notificationTime10);
-        row.add(PageLabels.notificationTime11);
-
-        callback.add(row);
-
-        row = new KeyboardRow();
-
-        row.add(PageLabels.notificationTime12);
-        row.add(PageLabels.notificationTime13);
-        row.add(PageLabels.notificationTime14);
-
-        callback.add(row);
-
-        row = new KeyboardRow();
-
-        row.add(PageLabels.notificationTime15);
-        row.add(PageLabels.notificationTime16);
-        row.add(PageLabels.notificationTime17);
-
-        callback.add(row);
-
-        row = new KeyboardRow();
-
-        row.add(PageLabels.notificationTime18);
-        row.add(PageLabels.notificationTimeOff);
-
-        callback.add(row);
-
-        keyboardMarkup.setKeyboard(callback);
-
-        InlineKeyboardMarkup inlineKeyboardMarkup = ButtonService.sendButtonMessageKeyboardRow(nameOfButtons, callback);
+        InlineKeyboardMarkup inlineKeyboardMarkup = ButtonService.sendButtonMessage(nameOfButtons, callback);
         SendMessage sendMessage = BotService.sendMessage(update.getCallbackQuery().getMessage().getChatId(), "Обери годину, о котрій щоденно отримуватимеш актуальний курс валют:");
-        sendMessage.setReplyMarkup(keyboardMarkup);
         sendMessage.setReplyMarkup(inlineKeyboardMarkup);
-
         return sendMessage;
     }
 }
